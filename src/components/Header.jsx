880e06e3fa2c2085b0b8f819d9248c11850b3aff
Greg Trahan <trahan.greg@gmail.com>
2023-03-03 10:03:20 -0600

    set up react

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react"
-import { LoginButton1, LoginButton2, LoginButton3 } from '.';
 
-const Header = ({isLoggedIn, setIsLoggedIn, activeLoginBtn}) => {
+const Header = () => {
   const [ showDropDown, setShowDropDown ] = useState(false)
 
   return (
-    <header style={{ borderBottom: "1px solid #333" }}>
+    <header className="px-2 pb-0 mb-0" style={{ borderBottom: "1px solid #333" }}>
       <nav className="navbar navbar-dark navbar-expand-md bg-body-secondary" data-bs-theme="dark">
         <div className="container-fluid">
           {/* <a className="navbar-brand" href="##">Navbar</a> */}
@@ -18,34 +17,9 @@ const Header = ({isLoggedIn, setIsLoggedIn, activeLoginBtn}) => {
                 <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/map">Array Map</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/api">Api Page</a>
-              </li>
-              <li className="nav-item dropdown">
-                <a 
-                  className="nav-link dropdown-toggle" 
-                  href="##" 
-                  role="button" 
-                  data-bs-toggle="dropdown" 
-                  aria-expanded={showDropDown}
-                  onClick={() => setShowDropDown(!showDropDown)}
-                >
-                  Form Examples
-                </a>
-                <ul className={ !showDropDown ? "dropdown-menu" : "dropdown-menu show" }>
-                  <li><a className="dropdown-item" href="/simple-form">Simple Form</a></li>
-                  <li><a className="dropdown-item" href="/complex-form">Complex Form</a></li>
-                </ul>
+                <a className="nav-link" href="/about">About Page</a>
               </li>
             </ul>
-
-            <div className="d-flex">
-              { activeLoginBtn === 1 && <LoginButton1 isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}
-              { activeLoginBtn === 2 && <LoginButton2 isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}
-              { activeLoginBtn === 3 && <LoginButton3 isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />}
-            </div>
           </div>
         </div>
       </nav>
